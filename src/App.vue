4c40b2b4caa044e5f365a37e55533df61f99e4a2
annet4588 <annet4588@yahoo.co.uk>
2024-01-17 22:11:42 +0000

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,47 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img alt="Calculator logo" src="./assets/calculator.png">
+ 
+  <h1>Compute Product</h1>
+  <div>
+    <label for num1>Number 1: </label>
+    <input type="number" v-model="num1"/>
+  </div>
+  <br>
+  <div>
+    <label for num2>Number 2: </label>
+    <input type="number" v-model="num2"/>
+  </div>
+  <br>
+  <button v-if="showButton" @click="multiplyNumbers">Multiply</button>
+  <br><br>
+  <div v-if="!showButton">
+    <label for result>Result: </label>
+    <input  type="result" v-model="result" readonly/>
+  </div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import {ref} from 'vue'
+export default{
+  setup(){
+    const num1 = ref(0)
+    const num2 = ref(0)
+    const result = ref(0)
+    const showButton = ref(true)
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+    const multiplyNumbers = () => {
+      
+      showButton.value = false;
+      result.value = num1.value * num2.value;
+    }
+    return {
+      num1, num2, result, showButton, multiplyNumbers
+    }
   }
+  
 }
+
 </script>
 
 <style>
